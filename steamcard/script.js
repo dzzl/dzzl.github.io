cecf0f67196d0f258a0bfee9f04a5dde919f4560
dzzl <dzzl@users.noreply.github.com>
2020-11-29 21:01:58 +0000

    Update script.js

--- steamcard/script.js
+++ steamcard/script.js
@@ -1,31 +1,103 @@
-const degreeExtremes = 10;
+class Card {
 
-document.querySelectorAll('card').forEach(el => {
+    constructor(el) {
 
-    const cardImage = el.querySelector('img');
+        this.element = el;
 
-    const setTransform = (x, y, z) => cardImage.style.transform = `rotateY(${x}deg) rotateX(${y}deg) translateZ(${z}px)`;
+        this.card = this.element.querySelector('.image');
 
-    el.addEventListener('mousemove', event => {
+        this.degreeExtremes = 10;
+    
+        this.cardWidth = (this.card.clientWidth * this.degreeExtremes);
+        this.cardHeight = (this.card.clientHeight * this.degreeExtremes);
 
-        cardImage.style.transition = `0s`;
+        this.isMoving = false;
+        this.mouseOut = true;
 
-        const cardWidth = (el.clientWidth * degreeExtremes);
-        const cardHeight = (el.clientHeight * degreeExtremes);
+        this.matrix = {
+            x: 0,
+            y: 0,
+            z: 0
+        }
 
-        let Xval = ((event.offsetX / cardWidth) * 100) - (degreeExtremes / 2);
-        let Yval = ((event.offsetY / cardHeight) * 100) - (degreeExtremes / 2);
-        const Zval = 60;
+        this.bindEvents();
 
+    }
+
+    bindEvents() {
+        this.element.addEventListener('mousemove', event => this.onMouseMove(event));
+        this.element.addEventListener('mouseout', event => this.onMouseOut(event));
+        this.element.addEventListener('mouseenter', event => this.onMouseEnter(event));
+    }
+
+    onMouseOut() {
+
+        this.mouseOut = true;
+
+        this.card.style.transition = `0.5s`;
+
+        this.matrix = {
+            x: 0,
+            y: 0,
+            z: 0
+        }
+
+        this.doMove();
+    }
+
+    onMouseMove(event) {
+
+        this.card.style.transition = `0s`;
+
+        let Xval = ((event.offsetX / this.cardWidth) * 100) - (this.degreeExtremes / 2);
+        let Yval = ((event.offsetY / this.cardHeight) * 100) - (this.degreeExtremes / 2);
         Yval = -Yval;
 
-        setTransform(Xval, Yval, Zval);
+        this.matrix.x = Xval;
+        this.matrix.y = Yval;
 
-    })
+        if (this.isMoving !== true){
+            this.doMove();
+        }
 
-    el.addEventListener('mouseout', event => {
-        cardImage.style.transition = `0.5s`;
-        setTransform(0, 0, 0);
-    })
+    }
 
+    onMouseEnter() {
+
+        this.mouseOut = false;
+        this.isMoving = true;
+
+        const step = () => {
+            this.matrix.z = (this.matrix.z + 10);
+            if ((this.matrix.z < 100) && (this.mouseOut === false)) {
+                window.requestAnimationFrame(step);
+                this.doMove();
+            } else {
+                this.isMoving = false;
+            }
+        }
+
+        window.requestAnimationFrame(step);
+
+    }
+
+    doMove() {
+
+        
+
+        let x = this.matrix.x;
+        let y = this.matrix.y;
+        let z = this.matrix.z;
+
+        this.card.style.transform = `rotateY(${x}deg) rotateX(${y}deg) translateZ(${z}px)`;
+
+    }
+
+}
+
+
+
+
+document.querySelectorAll('card').forEach(el => {
+    let card = new Card(el);
 })
